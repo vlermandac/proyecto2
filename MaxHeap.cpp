e8505427615e6b21a1dcf22a3d0afdff64d9beb2
Vicente <vicentelcandia@gmail.com>
2020-07-24 16:29:45 -0400

    a

--- MaxHeap.cpp
+++ MaxHeap.cpp
@@ -1,13 +1,13 @@
-#define first key
-#define second val
+#include "MaxHeap.h"
+
 MaxHeap::MaxHeap(){
-	seq.push_back(pair<int,ii>(-1,ii(0,0)));
+	seq.push_back(hnode(0,ii(0,0)));
 	hsize = 0;
 }
 
-~MaxHeap::MaxHeap(){}
+MaxHeap::~MaxHeap(){}
 
-int MaxHeap::parent(int){
+int MaxHeap::parent(int i){
 	return i>>1;
 }
 
@@ -21,7 +21,7 @@ int MaxHeap::right(int i){
 
 void MaxHeap::upheap(int i){
 	if(i == 1) return;
-	if(seq[i] > seq[parent(i)]) {
+	if(seq[i].key > seq[parent(i)].key) {
 		swap(seq[i], seq[parent(i)]);
 		upheap(parent(i));
 	}
@@ -30,9 +30,9 @@ void MaxHeap::upheap(int i){
 void MaxHeap::downheap(int i){
 	if(i > hsize) return;
 	int aux = i;
-	if(left(i) <= hsize && seq[i] < seq[left(i)])
+	if(left(i) <= hsize && seq[i].key < seq[left(i)].key)
 		aux = left(i);
-	if(right(i) <= hsize && seq[aux] < seq[right(i)])
+	if(right(i) <= hsize && seq[aux].key < seq[right(i)].key)
 		aux = right(i);
 	if(aux != i) {
 		swap(seq[i], seq[aux]);
@@ -40,3 +40,25 @@ void MaxHeap::downheap(int i){
 	}
 
 }
+
+void MaxHeap::push(hnode x) {
+	if(hsize + 1 >= (int)seq.size()) {
+		seq.push_back(hnode(0, ii(0,0)));
+	}
+	seq[++hsize] = x;
+	upheap(hsize);
+}
+
+void MaxHeap::pop() {
+	swap(seq[1], seq[hsize--]);
+	downheap(1);
+}
+
+hnode MaxHeap::top() {
+	return seq[1];
+}
+
+bool MaxHeap::empty() {
+	return hsize == 0;
+}
+
--- MaxHeap.h
+++ MaxHeap.h
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
-typedef pair<int,int> ii;
 using namespace std;
+typedef pair<int,int> ii;
+
+struct hnode{
+	int key;
+	ii val;
+	hnode(int a, ii b){ a = key; b = val; }
+};
+
 
 class MaxHeap{
 	private:
-		vector<pair<int,ii>> seq;
+		vector<hnode> seq;
 		int hsize;
 		int parent(int);
 		int left(int);
@@ -12,28 +19,11 @@ class MaxHeap{
 		void upheap(int);
 		void downheap(int);
 	public:
-		binary_heap();
-		void insert(int x) {
-			if (hsize+1 >= (int)seq.size()) {
-				seq.push_back(0);
-			}
-			seq[++hsize] = x;
-			upheap(hsize);
-		}
-
-		int extract_max() {
-			int taken = seq[1];
-			swap(seq[1], seq[hsize--]);
-			downheap(1);
-			return taken;
-		}
-
-		int get_max() {
-			return seq[1];
-		}
-
-		bool empty() {
-			return hsize == 0;
-		}
+		MaxHeap();
+		~MaxHeap();
+		void push(hnode);
+		void pop();
+		hnode top();
+		bool empty();
 };
 
